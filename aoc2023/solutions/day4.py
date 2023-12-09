9b0d802635c28c1bf2faef27fe2ef851505d0683
niku <nvdpoel01@gmail.com>
2023-12-09 12:41:41 +0100

    Added 2023 day 4 part 2

--- aoc2023/solutions/day4.py
+++ aoc2023/solutions/day4.py
@@ -1,4 +1,6 @@
 import re
+import typing
+from collections import defaultdict
 
 from utils import load_input
 
@@ -19,21 +21,39 @@ def solution_part1(input_data: str, winning_count: int = 5, play_count: int = 8)
     return result
 
 
+def solution_part2(input_data: str, winning_count: int = 5, play_count: int = 8) -> int:
+    card_counts: typing.DefaultDict[int, int] = defaultdict(int)
+    scratch_cards = input_data.splitlines()
+    play_offset = 1 + winning_count
+    for idx, line in enumerate(scratch_cards):
+        numbers = re.findall(r"\d+", line)
+        streak = 0
+        for i in range(play_offset, play_offset + play_count):
+            if numbers[i] in numbers[1:play_offset]:
+                streak += 1
+
+        if streak > 0:
+            extras = card_counts[idx]
+            for i in range(streak):
+                card_counts[idx + i + 1] += 1 + extras
+
+    return sum(card_counts.values()) + len(scratch_cards)
+
+
 def test_part_one_example():
-    input_data = (
-        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n"
-        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n"
-        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n"
-        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n"
-        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n"
-        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n"
-    )
+    input_data = load_input("day_4_test")
     assert solution_part1(input_data) == 13
+
+
+def test_part_two_example():
+    input_data = load_input("day_4_test")
+    assert solution_part2(input_data) == 30
 
 
 def main():
     input_data = load_input("day_4")
     print(f"Answer to day 4 part 1: {solution_part1(input_data, 10, 25)}")
+    print(f"Answer to day 4 part 2: {solution_part2(input_data, 10, 25)}")
 
 
 if __name__ == "__main__":
